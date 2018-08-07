3ee9a3af53b66bec9ed1abdb53233da80af3badf
Seulki Shin <sskcorea@gmail.com>
2018-08-07 07:49:34 +0000

    merge ffmpeg script to the main file
    Signed-off-by: Seulki Shin <sskcorea@gmail.com>

--- index.js
+++ index.js
@@ -8,19 +8,12 @@ var fs = require('fs'),
 	http = require('http'),
 	WebSocket = require('ws');
 	express = require("express");
-
-if (process.argv.length < 3) {
-	console.log(
-		'Usage: \n' +
-		'node websocket-relay.js <secret> [<stream-port> <websocket-port>]'
-	);
-	process.exit();
-}
+const { exec } = require('child_process');
 
 var STREAM_SECRET = process.argv[2],
 	STREAM_PORT = process.argv[3] || 8081,
 	WEBSOCKET_PORT = process.argv[4] || 8082,
-	RECORD_STREAM = false;
+	RECORD_STREAM = process.argv[5] || false;
 
 // Websocket Server
 var socketServer = new WebSocket.Server({port: WEBSOCKET_PORT, perMessageDeflate: false});
@@ -28,7 +21,7 @@ socketServer.connectionCount = 0;
 socketServer.on('connection', function(socket, upgradeReq) {
 	socketServer.connectionCount++;
 	console.log(
-		'New WebSocket Connection: ', 
+		'New WebSocket Connection: ',
 		(upgradeReq || socket.upgradeReq).socket.remoteAddress,
 		(upgradeReq || socket.upgradeReq).headers['user-agent'],
 		'('+socketServer.connectionCount+' total)'
@@ -52,17 +45,9 @@ socketServer.broadcast = function(data) {
 var streamServer = http.createServer( function(request, response) {
 	var params = request.url.substr(1).split('/');
 
-	if (params[0] !== STREAM_SECRET) {
-		console.log(
-			'Failed Stream Connection: '+ request.socket.remoteAddress + ':' +
-			request.socket.remotePort + ' - wrong secret.'
-		);
-		response.end();
-	}
-
 	response.connection.setTimeout(0);
 	console.log(
-		'Stream Connected: ' + 
+		'Stream Connected: ' +
 		request.socket.remoteAddress + ':' +
 		request.socket.remotePort
 	);
@@ -81,7 +66,7 @@ var streamServer = http.createServer( function(request, response) {
 
 	// Record the stream to a local file?
 	if (RECORD_STREAM) {
-		var path = 'recordings/' + Date.now() + '.ts';
+		var path = Date.now() + '.ts';
 		request.socket.recording = fs.createWriteStream(path);
 	}
 }).listen(STREAM_PORT);
@@ -91,8 +76,15 @@ app.use(express.static('.'))
 app.get('/',function(req,res){
 	res.sendFile('index.html');
 });
-
 app.listen(80);
+
+exec('ffmpeg -f v4l2 -framerate 25 -video_size 640x480 -i /dev/video6 -f mpegts -codec:v mpeg1video -s 640x480 -b:v 1000k -bf 0 http://localhost:8081/supersecret', (err, stdout, stderr) => {
+	if (err) throw err;
+
+	// the *entire* stdout and stderr (buffered)
+	console.log(`stdout: ${stdout}`);
+	console.log(`stderr: ${stderr}`);
+});
 
 console.log('Listening for incomming MPEG-TS Stream on http://127.0.0.1:'+STREAM_PORT+'/<secret>');
 console.log('Awaiting WebSocket connections on ws://127.0.0.1:'+WEBSOCKET_PORT+'/');
